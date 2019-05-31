909fea837fa59dc8df5f8eee200ee6accb5f1c3c
Yocoyani Hernandez <yocoyanihernandez@gmail.com>
2019-05-31 00:36:08 -0500

    Initial setup

---
+++ YocoAutomation/es-US/Music/esUS_PlayMusicFactory.java
@@ -0,0 +1,11 @@
+import good.music.Factory;
+import crazy.guitars.Factory;
+import nice.voices.DataProvider;
+
+public class PlayMusicFactory {
+	public Object[][] testList() {
+		return new Object[][] {
+			{"playMusicByArtist"}
+		};
+	}
+}
